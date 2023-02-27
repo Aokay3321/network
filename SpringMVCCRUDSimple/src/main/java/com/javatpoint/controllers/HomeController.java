b2e2b6623b308d05224463a59c56aacbd8b494e5
sarfaraz-1213 <121170874+sarfaraz-1213@users.noreply.github.com>
2023-02-27 17:02:42 +0530

    First commit from faraz

--- SpringMVCCRUDSimple/src/main/java/com/javatpoint/controllers/HomeController.java
+++ SpringMVCCRUDSimple/src/main/java/com/javatpoint/controllers/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 	public String home() {
 		
 		System.out.println("hello");
-		System.out.println("Hello Pushed By faraz");
+		System.out.println("Hello Pushed By faraz shaikh");
 		
 		return "home";
 		
